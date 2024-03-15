edf359de1fb26aa9bcca8e7daa4ea1066e977eee
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-03-15 00:25:42 -0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['http2.mlstatic.com'],
+    },
+  };
+
+module.exports = nextConfig
+
---
+++ src/app/projects/page.jsx
@@ -0,0 +1,6 @@
+import ProjectsPage from "@/views/ProjectsPage/ProjectsPage"
+
+
+export default function budgets() {
+  return (<ProjectsPage/>)
+}
