1201da17aac3c3387a2af153bb79c015a31260bf
EzequielTartaglia <ezequieltartaglia@churchofjesuschrist.org>
2024-07-10 14:06:20 -0300

    certifications

--- components/buttons/CallToActionButton.jsx
+++ components/buttons/CallToActionButton.jsx
@@ -14,7 +14,7 @@ export default function CallToActionButton({ route, text, openInNewTab }) {
 
   return (
     <Link {...linkProps}>
-      <div className="flex items-center">
+      <div className="flex items-center mt-9">
         <span className="relative inline-flex overflow-hidden rounded-full p-[1px]">
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#51E4B8_0%,#21554E_50%,#51E4B8_100%)]"></span>
           <div className="inline-flex items-center justify-center px-3 py-1 text-sm text-green-800 bg-green-100 rounded-full cursor-pointer dark:bg-gray-800 dark:text-white/80 backdrop-blur-3xl whitespace-nowrap">
