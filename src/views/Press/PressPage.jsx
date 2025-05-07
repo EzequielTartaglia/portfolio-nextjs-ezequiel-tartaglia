249ce1d912516789dd2f4c215cce07b0625e7b27
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2025-05-07 11:30:12 -0300

    Add Views

--- src/views/Press/PressPage.jsx
+++ src/views/Press/PressPage.jsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import PageBody from "@/components/page_formats/PageBody";
+import PageHeader from "@/components/page_formats/PageHeader";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNewspaper } from "@fortawesome/free-solid-svg-icons";
+
+// Logos
+import laNacionLogo from "../../../public/assets/press/la_nacion_logo.jpg";
+
+// Datos agrupados por tipo
+const revistas = [
+  {
+    title:
+      "Prompting, ética y entrenamiento de la IA: lo que todos debemos saber antes de interactuar con una inteligencia artificial",
+    outlet: "La Nación (Revista Ohlala!)",
+    date: "Junio 2025",
+    logo: laNacionLogo,
+    link: "",
+  },
+];
+
+
+const PressCard = ({ item }) => (
+  <div className="w-full md:w-1/2 p-4">
+    <a
+      href={item.link || "#"}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="block bg-gray-900 p-6 rounded-lg shadow-lg border border-secondary-light hover:bg-gray-800 transition-all"
+    >
+      <div className="flex items-center mb-4">
+        <Image
+          src={item.logo}
+          alt={`${item.outlet} Logo`}
+          className="h-12 w-12 mr-3"
+        />
+        <h3 className="text-xl font-bold text-white">{item.title}</h3>
+      </div>
+      <p className="text-sm text-gray-400">{item.date}</p>
+      <p className="text-sm text-gray-400">{item.outlet}</p>
+    </a>
+  </div>
+);
 
 export default function PressPage() {
   return (
-    <div>PressPage</div>
-  )
+    <>
+      <PageHeader
+        title="Apariciones en Prensa y Medios"
+        subtitle="Entrevistas, notas y participaciones en medios de comunicación."
+        goBackRoute="/"
+        goBackText="Volver al Inicio"
+      />
+      <PageBody>
+        <div className="bg-gray-900 border-t-4 border-teal-500 p-10 mt-8 border-secondary-light">
+        <h2 className="text-3xl text-teal-500 mb-6 flex">
+            <FontAwesomeIcon
+              icon={faNewspaper}
+              className="w-8 h-8 mr-2 text-teal-400"
+            />
+            Notas en Revistas o Diarios
+        </h2>
+          <div className="flex flex-wrap">
+            {revistas.map((item, idx) => (
+              <PressCard key={idx} item={item} />
+            ))}
+          </div>
+        </div>
+
+      </PageBody>
+    </>
+  );
 }
